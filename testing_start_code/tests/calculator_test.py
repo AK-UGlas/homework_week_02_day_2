2db45cf5afc26a8944c0d34432126d2084f62ca4
Allen_K <allenkelly278@gmail.com>
2021-04-06 21:03:26 +0100

    implement test_add function for calculator

--- testing_start_code/tests/calculator_test.py
+++ testing_start_code/tests/calculator_test.py
@@ -1,2 +1,14 @@
 import unittest
 from src.calculator import add, divide, multiply, subtract
+
+class TestCalculator(unittest.TestCase):
+    
+    def setUp(self):
+        self.first_num = 5
+        self.second_num = 8
+    
+    def test_add(self):
+        self.assertEqual(13, add(self.first_num, self.second_num))
+
+    
+
